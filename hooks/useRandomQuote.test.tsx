3993f780faa457200b41557c259cd27cd4631dc3
Rodolpheanger <rodolphe.anger@yahoo.fr>
2023-04-25 12:14:06 +0200

    🧪 test(useRandomQuote.ts): check if fetchRandomQuote function return awaited values

--- hooks/useRandomQuote.test.tsx
+++ hooks/useRandomQuote.test.tsx
@@ -1,7 +1,12 @@
-import { test } from "vitest";
+import { describe, test } from "vitest";
 import { fetchRandomQuote } from "~/hooks/useRandomQuote";
 
-test("fetch random quote", async () => {
-  const quote = await fetchRandomQuote();
-  expect(quote.randomQuote.text).toEqual("Excalibur");
+describe("useRandomQuote", () => {
+  test("should fetch a random quote", async () => {
+    const quote = await fetchRandomQuote();
+    expect(quote.randomQuote.id).toBe(1);
+    expect(quote.randomQuote.text).toBe("Excalibur");
+    expect(quote.randomCharacters[0].characters_id).toBe(1);
+    expect(quote.randomCharacters[0].characters_name).toBe("Good character");
+  });
 });

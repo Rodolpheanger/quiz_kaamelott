28369d5f0ca35e2b61a3c355934c89b3a514eeb5
Rodolpheanger <rodolphe.anger@yahoo.fr>
2023-04-25 12:11:54 +0200

    🧪 test(quiz/page.tsx): check if RandomQuote & ChoicesForm are mounted

--- app/quiz/page.test.tsx
+++ app/quiz/page.test.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import { screen } from "@testing-library/react";
 import Quiz from "./page";
 import { customRender } from "~/test/utils";
+import { expect } from "vitest";
 
 test("renders RandomQuote and ChoicesForm components", async () => {
   customRender(<Quiz />);
   screen.debug();
+  const RandomQuote = screen.getByText(/patiente/i);
+  const ChoicesForm = screen.getByRole("button", { name: /valider/i });
+  expect(RandomQuote).toBeInTheDocument();
+  expect(ChoicesForm).toBeInTheDocument();
 });
